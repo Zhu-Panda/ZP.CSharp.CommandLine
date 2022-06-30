778922a121726a74cb4fc0a381075c95baf4d9a5
Zhu Panda <zhupandahugo@gmail.com>
2022-06-30 19:36:06 +0800

    add test program

---
+++ ZP.CSharp.CommandLine.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using ZP.CSharp.CommandLine.Builder;
+using ZP.CSharp.CommandLine.Tests;
+namespace ZP.CSharp.CommandLine.Tests
+{
+    public class Program
+    {
+        public static RootCommand Command = new RootCommand();
+        public static CommandLineBuilder Builder = new CommandLineBuilder(Command);
+        public static Parser Parser = Builder.UseDefaultsWithHelp("-?", "--help").Build();
+        public static void Main(string[] args) => Parser.Invoke("-?");
+    }
+}
