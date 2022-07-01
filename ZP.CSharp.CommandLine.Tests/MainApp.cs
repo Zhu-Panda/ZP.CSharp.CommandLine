d7d5ae5eb417cb6a598edaaf7123a470cdb3ca38
Zhu Panda <zhupandahugo@gmail.com>
2022-07-01 14:26:03 +0800

    split logic

---
+++ ZP.CSharp.CommandLine.Tests/MainApp.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Text;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using ZP.CSharp.CommandLine.Builder;
+using ZP.CSharp.CommandLine.Strategies.InvokeParser;
+using ZP.CSharp.CommandLine.Tests;
+namespace ZP.CSharp.CommandLine.Tests
+{
+    public class MainApp : IParserInvoker
+    {
+        public Command Command
+        {
+            get => CommandFactory.NewRoot()
+                .WithOption(new Option<bool>(new[]{"-v", "--verbose"}, "verbose"), true)
+                .WithCommand(CommandFactory.New("ha", "To ha or not to ha, that's the question.")
+                    .WithArgument(new Argument<int>("times", "times to ha"))
+                    .WithOption(new Option<string>("--lang", "language to ha in"))
+                    .WithHandler(MainApp.Ha))
+                .ToRoot();
+        }
+        public CommandLineBuilder Builder
+        {
+            get => new CommandLineBuilder(this.Command);
+        }
+        public Parser Parser
+        {
+            get => this.Builder.UseDefaultsWithHelp("-?", "--help").Build();
+        }
+        public static void Ha(int times, string lang)
+        {
+            Console.WriteLine($"Language: {lang}\n");
+            StringBuilder hahahas = new();
+            for (int i = 0; i < times; i++)
+            {
+                hahahas.Append("ha ");
+            };
+            Console.WriteLine(hahahas);
+        }
+        public int Invoke(string[] args)
+        {
+            return this.Parser.Invoke(args);
+        }
+        public async Task<int> InvokeAsync(string[] args)
+        {
+            return await new Task<int>(() => 0);
+        }
+    }
+}
--- ZP.CSharp.CommandLine.Tests/Program.cs
+++ ZP.CSharp.CommandLine.Tests/Program.cs
@@ -1,15 +1,10 @@
-﻿using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Parsing;
-using ZP.CSharp.CommandLine.Builder;
+﻿using System;
 using ZP.CSharp.CommandLine.Tests;
 namespace ZP.CSharp.CommandLine.Tests
 {
     public class Program
     {
-        public static RootCommand Command = new RootCommand();
-        public static CommandLineBuilder Builder = new CommandLineBuilder(Command);
-        public static Parser Parser = Builder.UseDefaultsWithHelp("-?", "--help").Build();
-        public static void Main(string[] args) => Parser.Invoke("-?");
+        public static void Main(string[] args) => new MainApp().Invoke(args);
     }
+    
 }
