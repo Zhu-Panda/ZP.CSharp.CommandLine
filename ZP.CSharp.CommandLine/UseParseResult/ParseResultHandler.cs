ea11bb09440ce27358cb4475c9ffcee8682ec647
Zhu Panda <zhupandahugo@gmail.com>
2022-07-03 15:18:00 +0800

    add helper class

---
+++ ZP.CSharp.CommandLine/UseParseResult/ParseResultHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using ZP.CSharp.CommandLine.UseParseResult;
+namespace ZP.CSharp.CommandLine.UseParseResult
+{
+    public abstract class ParseResultHandler<T> : CommandLineAppComponent<T>, IParseResultHandler
+        where T : ParseResultHandler<T>, new()
+    {
+        #pragma warning disable CS8618
+        public ParseResult ParseResult {get; set;}
+        #pragma warning restore CS8618
+        public static new T FromArgs(string[] args)
+        {
+            return new T(){CommandLineArgs = args}.SetParseResult();
+        }
+        private T SetParseResult()
+        {
+            this.ParseResult = this.Parser.Parse(this.CommandLineArgs);
+            return ((T) this);
+        }
+    }
+}
